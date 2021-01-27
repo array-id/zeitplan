9a1828817d301f27c4e35d45525ac79dd85075cc
rizalfakhri <me@rizalfakhri.id>
2021-01-28 00:09:40 +0800

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,84 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+    <>
+      <nav className="flex items-center justify-between w-full h-20 lg:px-64">
+        <div className="flex items-center w-20 h-20 py-5 -mx-2 lg:w-4/12">
+          <img src="/logo.png" alt="Zeitplan Logo" className="w-auto h-auto px-2"/>
+          <h3 className="px-2 text-2xl font-bold">ZEITPLAN</h3>
+        </div>
+        <div className="flex items-center w-4/12 -mx-7">
+          <div className="relative mx-7">
+            <a href="#" className="font-semibold text-tomato-500">Home</a>
+            <div className="absolute w-1 h-1 mt-1 rounded-full bg-tomato-300 inset-x-5"></div>
+          </div>
+          <div className="relative mx-7">
+            <a href="#" className="font-sans font-light text-gray-400">Features</a>
+          </div>
+          <div className="relative mx-7">
+            <a href="#" className="font-sans font-light text-gray-400">About</a>
+          </div>
+          <div className="relative mx-7">
+            <a href="#" className="font-light text-gray-400 sans">Contact</a>
+          </div>
+        </div>
+        <div className="flex items-center w-20 h-20 py-5 lg:w-4/12">
+          <a  href="#" className="ml-auto">
+            <div className="flex py-2 rounded-lg px-7 bg-tomato-500">
+                <h4 className="text-white">Download</h4>
+                <div className="w-5 h-5 my-auto ml-2">
+                  <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.167 10.833h9.883l-3.025 3.634a.834.834 0 101.283 1.066l4.167-5a.987.987 0 00.075-.125c0-.041.042-.066.058-.108a.83.83 0 00.059-.3.83.83 0 00-.059-.3c0-.042-.041-.067-.058-.108a.996.996 0 00-.075-.125l-4.167-5a.833.833 0 00-1.175-.109.832.832 0 00-.108 1.175l3.025 3.634H4.167a.833.833 0 100 1.666z" fill="#fff"/>
+                  </svg>
+                </div>
+            </div>
           </a>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </nav>
+      <main className="flex items-center justify-between my-20 lg:px-64">
+        <div className="w-1/2">
+          <h1 className="font-serif text-6xl">The easiest way to manage college schedules</h1>
+          <div className="mt-5 ">
+            <p className="text-sm font-light text-gray-500">Datang kuliah terlambat karena ketiduran itu tidak masalah;</p>
+            <p className="text-sm font-light text-gray-500">Datang kuliah karena lupa jadwal itu sangat tidak keren.</p>
+          </div>
+          <div className="flex items-center my-5 -mx-5">
+            <button className="px-10 py-3 mx-5 text-white rounded-lg bg-tomato-500">Get Started</button>
+            <a href="#" className="flex mx-5 my-auto">
+              <div className="w-5 h-5 my-auto mr-3">
+                <svg width="100" height="100" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.703 10.123a2 2 0 01-.076 3.341l-9.565 5.98c-1.332.834-3.06-.124-3.06-1.695V5.165c0-1.612 1.81-2.562 3.136-1.646l9.565 6.604z" fill="#FE5E44"/></svg>
+              </div>
+              <h5 className="text-tomato-500">Play Preview</h5>
+            </a>
+          </div>
+          <div className="mt-20">
+            <h5 className="text-sm text-gray-400 uppercase">Featured On</h5>
+            <div className="flex my-5 -mx-2">
+              <div className="w-auto h-5 mx-2 my-auto">
+                <img src="/cnn.png" alt=""/>
+              </div>
+
+              <div className="w-auto h-5 mx-2 my-auto">
+                <img src="/tirto.png" alt=""/>
+              </div>
+
+              <div className="w-auto h-5 mx-2 my-auto">
+                <img src="/forbes.png" alt=""/>
+              </div>
+
+              <div className="w-auto h-5 mx-2 my-auto">
+                <img src="/google.png" alt=""/>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <img src="/illustration.png" alt=""/>
+        </div>
+      </main>
+    </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+          colors: {
+            tomato: {
+              '50':  '#fbf7f3',
+              '100': '#fcede6',
+              '200': '#fad7cb',
+              '300': '#f8b69d',
+              '400': '#f88660',
+              '500': '#fe5e44',
+              '600': '#f13b24',
+              '700': '#d42c24',
+              '800': '#ac2425',
+              '900': '#8b1e21',
+            }
+        }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
